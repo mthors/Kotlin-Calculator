d6616dca3cb114b7a43c03dee28891408f3259f3
Thoriq <mtsthor@gmail.com>
2022-01-31 12:43:52 +0700

    Adding onClick Functionality and onClear Functionality

--- app/src/main/java/com/mtsthor/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/mtsthor/kotlincalculator/MainActivity.kt
@@ -3,15 +3,26 @@ package com.mtsthor.kotlincalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var screen: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        screen = findViewById(R.id.screen)
     }
 
     fun onDigit(view: View){
-        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        screen?.append((view as Button).text)
+    }
+
+    fun onClear(view: View){
+        screen?.text = ""
     }
 }
